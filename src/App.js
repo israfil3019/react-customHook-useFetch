9edc04b9b5a90cfa25040b5fdf62f22caa56c8d9
israfil <israfil3019@gmail.com>
2021-05-29 00:16:51 +0300

    add customHook lesson

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useFetch } from "./hooks/useFetch";
+import "./App.css";
+
+function App() {
+  const { response, loading, error } = useFetch(
+    "https://jsonplaceholder.typicode.com/users",
+    {}
+  );
+
+  return (
+    <div className="App">
+      <h2>Custom Hook</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error.message}</p>
+      ) : (
+        response?.map((result) => (
+          <p key={result?.id}>
+            {result?.id}-{result?.name}
+          </p>
+        ))
+      )}
+    </div>
+  );
+}
+
+export default App;
